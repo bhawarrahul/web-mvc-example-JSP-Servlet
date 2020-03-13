08da3309cad786ee631940fa6f1a5560161ece00
Rahul Bhawar <bhawar.rahul7@gmail.com>
2020-03-13 14:15:52 +0530

    Update DatabaseConstants.java

--- src/main/java/com/project/utils/DatabaseConstants.java
+++ src/main/java/com/project/utils/DatabaseConstants.java
@@ -3,7 +3,7 @@ package com.project.utils;
 public class DatabaseConstants {
 	public static final String POSTGRES_DRIVER = "org.postgresql.Driver"; 
 	public static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/userdb";
-	public static final String DB_USER_NAME="globant";
-	public static final String DB_PASSWORD="globant";
+	public static final String DB_USER_NAME="root";
+	public static final String DB_PASSWORD="root";
 
 }
